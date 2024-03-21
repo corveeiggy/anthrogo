6d3bdac74a1e36d3b5294a3ccae973cbcc769753
Ignacio Bellomo <ignacio@corvee.com>
2024-03-21 15:36:33 -0400

    Add haiku model to models listing

--- anthropic_models.go
+++ anthropic_models.go
@@ -7,6 +7,7 @@ type AnthropicModel string
 const (
 	ModelClaude3Opus   AnthropicModel = "claude-3-opus-20240229"
 	ModelClaude3Sonnet AnthropicModel = "claude-3-sonnet-20240229"
+	ModelClaude3Haiku  AnthropicModel = "claude-3-haiku-20240307"
 
 	ModelClaude2     AnthropicModel = "claude-2"
 	ModelClaude2Dot1 AnthropicModel = "claude-2.1"
